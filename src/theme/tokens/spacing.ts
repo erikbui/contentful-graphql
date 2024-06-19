eb887e8a4f32db50c685ccdfc80ce39620c1689c
Erik Che Bui <erik.bui@knowit.no>
2024-06-19 09:24:12 +0200

    Adds spacing tokens

---
+++ src/theme/tokens/spacing.ts
@@ -0,0 +1,15 @@
+import { defineTokens } from "@pandacss/dev";
+
+export const spacing = defineTokens.spacing({
+  "3xs": { value: "0.25rem" },
+  "2xs": { value: "0.375rem" },
+  xs: { value: "0.5rem" },
+  sm: { value: "0.625rem" },
+  md: { value: "0.75rem" },
+  lg: { value: "1rem" },
+  xl: { value: "1.5rem" },
+  "2xl": { value: "2rem" },
+  "3xl": { value: "2.5rem" },
+  "4xl": { value: "4rem" },
+  "5xl": { value: "6rem" },
+});
