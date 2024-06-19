0b16ba0945c5db05b2bfb27c2fd99e9be288e539
Erik Che Bui <erik.bui@knowit.no>
2024-06-19 09:24:07 +0200

    Adds color tokens

---
+++ src/theme/tokens/colors.ts
@@ -0,0 +1,19 @@
+import { defineTokens } from "@pandacss/dev";
+
+export const colors = defineTokens.colors({
+  grayscale: {
+    0: { value: "#ffffff" },
+    50: { value: "#fafafa" },
+    100: { value: "#f5f5f5" },
+    200: { value: "#e5e5e5" },
+    300: { value: "#d6d6d6" },
+    400: { value: "#a4a2a2" },
+    500: { value: "#716f6f" },
+    600: { value: "#525151" },
+    700: { value: "#413f3f" },
+    800: { value: "#272626" },
+    900: { value: "#171616" },
+    950: { value: "#151414" },
+    1000: { value: "#0d0c0c" },
+  },
+});
