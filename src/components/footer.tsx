b018caaf4655be7f701457e0c4900bf723b29c0e
Erik Che Bui <erik.bui@knowit.no>
2024-06-19 09:24:46 +0200

    Adds header and footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,16 @@
+import { css } from "../../styled-system/css";
+
+export const Footer = () => (
+  <div
+    className={css({
+      display: "flex",
+      bg: "black",
+      h: "5rem",
+      justifyContent: "center",
+      alignItems: "center",
+      color: "white",
+    })}
+  >
+    Footer
+  </div>
+);
---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import { css } from "../../styled-system/css";
+
+export const Header = () => (
+  <div
+    className={css({
+      display: "flex",
+      bg: "#8acf01",
+      h: "4rem",
+      justifyContent: "center",
+      alignItems: "center",
+    })}
+  >
+    Header
+  </div>
+);
